3b073bfbcb6ba88add47973e37f7f6cecaeb16e6
Iago Sousa <fciagsousa94@gmail.com>
2019-02-16 18:33:24 -0300

    Update indexer.py
    adicionando indexador com separação por tags e ordenação por nível

--- indexer.py
+++ indexer.py
@@ -4,15 +4,18 @@
 import os
 
 base = [x[0] for x in os.walk("./base")]
-indexes = []
-for i in range(1,len(base)):
-    with open(base[i] + "/Readme.md", "r") as readme:
-        texto = readme.readlines()
-        indexes.append(("- [%s](%s/Readme.md#qxcode)\n" % (texto[1].strip()[3:], base[i])))
-
+tags = {'ope':[], 'sel':[], 'rep':[], 'vet':[], 'cha':[], 'str':[], 'mat':[], 'arq':[]}
 
 with open("./indice.md", "w+") as saida:
-    saida.write("# @qxcode\n\nArquivo a ser gerado automaticamente a partir dos títulos das questões.\n\n")
-    for linha in indexes:
-        saida.write(linha)
+    saida.write("# @qxcode\n\nArquivo a ser gerado automaticamente a partir dos títulos das questões.")
+    for i in range(1,len(base)):
+        with open(base[i] + "/Readme.md", "r") as readme:
+            texto = readme.readlines()
+            tags[texto[1].strip()[4:7]].append((("- [%s](%s/Readme.md#qxcode)\n" % (texto[1].strip()[3:], base[i]))))
 
+    for tag in tags:
+        tags[tag] = sorted(tags[tag], key=lambda x : int(x[18]))
+        saida.write("\n\n")
+        saida.write("## %s\n" % tag)
+        for questao in tags[tag]:
+            saida.write(questao)
